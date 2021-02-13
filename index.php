a5415b70bea66fe355138af8e8554db61a695692
koji <kinji.kamizaki@gmail.com>
2021-02-13 17:49:15 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,95 @@
+<?php
+header("Content-type: text/plain; charset=UTF-8");
+
+# input parameter from GET request
+$humid = $_GET['humid'];
+$temp = $_GET['temp'];
+$abs_hum = $_GET['abs_hum'];
+$CO2 = $_GET['co2'];
+$baro = $_GET['baro'];
+
+# setting parameters
+$datetime = date("Y/m/d_H:i:s");
+$string = $datetime . "," . $humid . "," . $temp . "," . $abs_hum . "," . $CO2 . "," . $baro . "\n";
+$header = "datetime,humidity[%],temperature[C],absolute_humidity[g/m3],CO2_density[ppm],barometer[Pa]\n";
+$lines = array();
+
+# setting Slack URL
+$url = file_get_contents("webhook.txt");
+$url = str_replace(array("\r","\n"),"",$url);
+
+# check output file
+if (!(file_exists("output.csv"))){
+	$fs = fopen("output.csv","w");
+	fwrite($fs,$header);
+	fwrite($fs,$string);
+	fclose($fs);
+} else {
+	# read output file for 1000 lines
+	$fs = fopen("output.csv","r");
+	$i = 0;
+	$line = fgets($fs);
+	while (($line = fgets($fs))){
+		if ($i > 999) {
+			break;
+		}
+		$lines[$i] = $line;
+		$i = $i + 1;
+	}
+	fclose($fs);
+
+	# write data to output file
+	$fs = fopen("output.csv","w");
+	fwrite($fs,$header);
+	fwrite($fs,$string);
+	foreach ($lines as &$row){
+		fwrite($fs,$row);
+	}
+	fclose($fs);
+}
+
+# create message
+$message = "";
+
+# sensor error check
+if ($humid == "" || $temp == "" || $abs_hum == "" || $CO2 == "" || $baro == "") {
+	$message = $message . "センサーエラーです。\n";
+}
+
+# alart for humidity
+if ($abs_hum > 15){
+	$message = $message . "部屋の湿度が高いです。\n絶対湿度は" . $abs_hum . "です。\n";
+} elseif ($abs_hum < 5){
+	$message = $message . "部屋が乾燥しています。\n絶対湿度は" . $abs_hum . "です。\n";
+}
+
+# alart for CO2 density
+if ($CO2 > 1500){
+	$message = $message . "部屋の二酸化炭素濃度が高いです。換気してください。\n二酸化炭素濃度は" . $CO2 . "ppmです。\n";
+}
+
+# create payload
+$payload = [
+	"text" => $message
+];
+
+# send to slack
+if ($message !== ""){
+	$ch = curl_init();
+	$options = array(
+		CURLOPT_URL => $url,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_SSL_VERIFYPEER => false,
+		CURLOPT_POST => true,
+		CURLOPT_POSTFIELDS => http_build_query(array(
+			'payload' => json_encode($payload)
+		))
+	);
+	curl_setopt_array($ch, $options);
+	curl_exec($ch);
+	curl_close($ch);
+}
+
+echo "hello";
+
+?>
